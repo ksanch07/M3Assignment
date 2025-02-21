9d3aa9f3856227f83dfab8ef18a59839296a59b4
Keoni Sanchez <keoni.sanchez@gmail.com>
2025-02-20 17:10:45 -0800

    4 commit message

--- js/04-grade-assigner.js
+++ js/04-grade-assigner.js
@@ -10,3 +10,23 @@
 // If the score is between 90 and 100, write out “You received an A” into the console.
 // If the score is less than 60, write out “You received an F” into the console.
 
+// Get number
+let grade = parseInt(prompt("Enter a grade between 1 and 100:"));
+
+// valid range check
+if (grade < 1 || grade > 100 || isNaN(grade)) {
+    console.log("Invalid grade. Please enter a number between 1 and 100.");
+} else {
+    // assign grades
+    if (grade >= 90) {
+        console.log("You received an A");
+    } else if (grade >= 80) {
+        console.log("You received a B");
+    } else if (grade >= 70) {
+        console.log("You received a C");
+    } else if (grade >= 60) {
+        console.log("You received a D");
+    } else {
+        console.log("You received an F");
+    }
+}
